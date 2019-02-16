bf727ac84762bd36c62f371e6ac4bbcf95bfcb21
Lewis Steele <lewis.steele@sykescottages.co.uk>
2019-02-16 14:10:06 +0000

    Got sql erros showing

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,83 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { execSync } from 'child_process';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const diagnosticCollectionName = 'sql-lint';
+let diagnosticCollection: vscode.DiagnosticCollection;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "vscode-sql-lint" is now active!');
+function activate(context: vscode.ExtensionContext): void {
+	console.log('Congratulations, your extension "vscode-sql-lint" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const config = vscode.workspace.getConfiguration('sql-lint');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	// const selector: vscode.DocumentSelector = {
+	// 	scheme: 'file',
+	// 	language: 'sql',
+	// };
+
+  	diagnosticCollection = vscode.languages.createDiagnosticCollection(
+		diagnosticCollectionName
+	);
+  	context.subscriptions.push(diagnosticCollection);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		console.log('did get here');
+
+		// if (event.languageId === 'sql') {
+		const path = event.document.uri.fsPath;
+
+		console.log('did get here 1');
+
+		const errors = runLinter(path, config);
+
+		diagnosticCollection.clear();
+
+		let diagnosticMap: Map<string, vscode.Diagnostic[]> = new Map();
+
+		errors.forEach(error => {
+			let canonicalFile = vscode.Uri.file(error.file).toString();
+			let range = new vscode.Range(
+				error.line - 1,
+				error.startColumn,
+				error.line - 1,
+				error.endColumn,
+			);
+			let diagnostics = diagnosticMap.get(canonicalFile);
+
+			if (!diagnostics) {
+				diagnostics = []; 
+			}
+
+			diagnostics.push(new vscode.Diagnostic(
+				range,
+				error.message,
+				error.severity,
+			));
+			diagnosticMap.set(canonicalFile, diagnostics);
+		});
+
+		diagnosticMap.forEach((diags, file) => {
+			diagnosticCollection.set(vscode.Uri.parse(file), diags);
+		});
 	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function runLinter(file: string, config: any): Array<any> {
+	const cmd = `sql-lint --host=${config.host} --user=${config.user} --password=${config.password} --file=${file}`;
+	const result = execSync(cmd).toString();
+
+	return [{
+		file: file,
+		line: 1,
+		message: result,
+		startColumn: 0,
+		endColumn: 100,
+	}];
+}
+
+function onChange() {
+	console.log('onChange did get called!!!!!!!');
+}
+
+function deactivate(): void { }
+
+export { activate, deactivate };
