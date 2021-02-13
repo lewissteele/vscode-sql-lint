68ec2d5925704a3302ba5de67154f0666aad3797
Lewis Steele <lewis.steele@hey.com>
2021-02-13 14:32:56 +0000

    Added standardjs code standard

---
+++ src/types/Event.ts
@@ -0,0 +1,4 @@
+export default interface Event {
+  languageId: string,
+  file: string,
+}
---
+++ test/test.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import { expect } from 'chai';
+
+describe('Test', () => {
+  it('Test', () => {
+    expect(true).to.be.equal(true);
+  });
+});
+
