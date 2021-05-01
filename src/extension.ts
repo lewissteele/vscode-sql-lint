bdee98e5c7feebc8d83c447984abcde6ec34be23
Lewis Steele <lewis.steele@hey.com>
2021-05-02 00:24:50 +0100

    🚧 Call sql-lint

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
+import { promisify } from 'util'
+import * as cd from 'child_process'
 import * as vscode from 'vscode'
 
 const diagnosticCollection = vscode.languages.createDiagnosticCollection()
+const exec = promisify(cd.exec)
 
 export function activate (context: vscode.ExtensionContext): void {
   vscode.workspace.onDidChangeTextDocument(handleEvent)
@@ -9,7 +12,7 @@ export function activate (context: vscode.ExtensionContext): void {
 
 export function deactivate (): void {}
 
-function handleEvent (event: any): void {
+async function handleEvent (event: any): Promise<void> {
   const path: string = event.document.uri.fsPath
 
   diagnosticCollection.clear()
@@ -22,5 +25,16 @@ function handleEvent (event: any): void {
     'something is wrong',
     vscode.DiagnosticSeverity.Error
   )
+
   diagnosticCollection.set(vscode.Uri.parse(path), [diagnostic])
+
+  await lint(path)
+}
+
+async function lint (file: string): Promise<void> {
+  const cmd = `sql-lint --format=json --host=localhost --user=test --password=test ${file}`
+
+  const output = await exec(cmd)
+
+  console.log(output)
 }
