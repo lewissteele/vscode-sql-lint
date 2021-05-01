493a9faf2a6d91fb289658f88b0d9ccc565ddd79
Lewis Steele <lewis.steele@hey.com>
2021-05-01 23:36:14 +0100

    🚧 Displaying an error on line

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const diagnosticCollection = vscode.languages.createDiagnosticCollection()
+
 export function activate (context: vscode.ExtensionContext): void {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "vscode-sql-lint" is now active!')
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand('vscode-sql-lint.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
-
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from vscode-sql-lint!')
-  })
-
-  context.subscriptions.push(disposable)
+  vscode.workspace.onDidChangeTextDocument(handleEvent)
+  vscode.workspace.onDidOpenTextDocument(handleEvent)
 }
 
-// this method is called when your extension is deactivated
 export function deactivate (): void {}
+
+function handleEvent (event: any): void {
+  const path: string = event.document.uri.fsPath
+
+  diagnosticCollection.clear()
+
+  const diagnostic = new vscode.Diagnostic(
+    new vscode.Range(
+      new vscode.Position(1, 1),
+      new vscode.Position(1, 1)
+    ),
+    'something is wrong',
+    vscode.DiagnosticSeverity.Error
+  )
+  diagnosticCollection.set(vscode.Uri.parse(path), [diagnostic])
+}
